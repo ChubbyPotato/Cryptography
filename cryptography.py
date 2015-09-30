9dce81821db6e0c3c18ee726ca42528eefad5514
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-09-30 14:01:00 -0400

    Updated from Brython Server: 9/30/2015 2:00:59 PM

--- cryptography.py
+++ cryptography.py
@@ -1,12 +1,17 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Suhan Gui
+Credit: Stack Overflow
 
-Assignment:
+Assignment: Cryptography
 
 Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+doge=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+
+for x in doge:
+    
