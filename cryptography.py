6c6819972007256dedfbd07b95c479b22ac0ec6b
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-10-16 14:19:15 -0400

    Updated from Brython Server: 10/16/2015 2:19:15 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
 Author: Suhan Gui
-Credit: Stack Overflow
+Credit: None
 
 Assignment: Cryptography
 
@@ -9,23 +9,27 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-
+#doge=(dict(map(lambda letter:(letter,len(potato)-len(potato.replace(letter,''))),potato)))
+#sorted_doge = sorted(doge.items(), key=operator.itemgetter(0))
+#converted_doge = list(doge.items())
 x = ""
 while x != "q":
     doge=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
     if doge=="q":
         print("Goodbye!")
         break
     elif doge=="e":
         potato=input("Message: ")
         Key=input("Key: ")
-        print("{0} = {1}".format(potato,encrypt))
-        encrypt=
+        print("{0} = {1}".format(potato,str(encrypt)))
+        for x in associations:
+            encrypt="doge"
     elif doge=="d":
         tomato=input("Message: ")
         key=input("Key: ")
-        print("{0} = {1}".format(tomato,decrypt))
-        decrypt=
+        print("{0} = {1}".format(tomato,str(decrypt)))
+        for x in associations:
+            decrypt="doge"
     else:
         print("Did not understand command, try again.")
