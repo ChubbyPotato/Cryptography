64120392418926d6b15853dbb336b51eb0cca293
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-10-19 14:26:12 -0400

    Updated from Brython Server: 10/19/2015 2:26:11 PM

--- cryptography.py
+++ cryptography.py
@@ -9,9 +9,6 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-#doge=(dict(map(lambda letter:(letter,len(potato)-len(potato.replace(letter,''))),potato)))
-#sorted_doge = sorted(doge.items(), key=operator.itemgetter(0))
-#converted_doge = list(doge.items())
 x = ""
 while x != "q":
     doge=input("Enter e to encrypt, d to decrypt, or q to quit: ")
@@ -22,19 +19,17 @@ while x != "q":
     elif doge=="e":
         potato=input("Message: ")
         Key=input("Key: ")
-        a = str(c)
-        b = (dict(map(lambda letter:(letter,len(potato)-len(potato.replace(letter,''))),potato)))
-        c = list(b.items())
-#            b=str(associations.find(a))
-#            a="a"
-        print(a)
+        print("{0}".format(int(decrypt)))
+        for x in potato:
+            b=str(associations.find(a))
+            a="d"
     elif doge=="d":
         tomato=input("Message: ")
         key=input("Key: ")
         print("{0}".format(int(decrypt)))
         for x in tomato:
-            decrypt=("B")
-#            B=associations.find(A)
-#            A="a"
+            decrypt=(B)
+            B=associations.find(A)
+            #A=
     else:
         print("Did not understand command, try again.")
